e3ea0d1d40e579d7435abeed49566ca2f0ade69d
ciel89 <155046322+ciel89@users.noreply.github.com>
2024-05-21 15:38:37 +0900

    Add files via upload

---
+++ jangmk/js/page.js
@@ -0,0 +1,23 @@
+window.onload = function() {
+
+    //페이지 이동
+    const wrap = document.getElementById('wrap');
+    const content = document.getElementsByClassName('content');
+    let page = 0;
+    const lastPage = content.length - 1;
+    
+    window.addEventListener('wheel', function(e){
+                                        e.preventDefault();
+                                        if (e.deltaY > 0) {page++;}
+                                        else if (e.deltaY < 0) {page--;}
+                                        //else {거짓}
+                                        console.log(e.deltaY);
+    
+                                        if (page < 0) {page = 0;}
+                                        else if (page > lastPage) {page = lastPage;}
+                                        //else {거짓}
+    
+                                        wrap.style.top = page * -100 + 'vh';
+                                    },{passive:false});
+    
+    }
